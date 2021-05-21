7183a08aa0e50ac86efa8969fd90ad4da116fcc2
Tejas Raj <raj.tejas2.tr@gmail.com>
2021-05-21 18:14:20 +0530

    stage 1 of updating update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from RegistrationGUI import register_customer
 from whatkit import send_message
+from temptree import *
 #import activation_change
 import csv
 from tkinter import *
@@ -12,12 +13,13 @@ root.title("Welcome")
 
 def change_data():
     return
-
-registration_button = Button(root, text="Register New Customer", padx = 10, pady = 20, command = register_customer)
+button_frame = Frame(root)
+button_frame.pack()
+registration_button = Button(button_frame, text="Register New Customer", padx = 10, pady = 20, command = register_customer)
 registration_button.grid(row = 0, column = 0)
-messaging = Button(root, text= "Send new Message", padx = 20, pady = 20, command = send_message)
+messaging = Button(button_frame, text= "Send new Message", padx = 20, pady = 20, command = send_message)
 messaging.grid(row = 0, column = 2)
-data_change = Button(root, text = "Change data", padx = 45, pady = 20, command = change_data)
+data_change = Button(button_frame, text = "Change data", padx = 45, pady = 20, command = update)
 data_change.grid(row = 0, column = 3)
 root.mainloop()
 
